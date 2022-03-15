832b761e46adc56776880bc033b39dd24a4581ab
YeonjooLEE08 <yeon__joo@naver.com>
2022-03-15 11:16:57 +0900

    1

---
+++ Board/src/main/java/com/kh/board/controller/BoardController.java
@@ -0,0 +1,19 @@
+package com.kh.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//@Controller 어노테이션의 역할
+//1.해당 클래스를 controller로 인식
+@Controller
+//@RequestMapping("/board")
+//-요청 경로가 /board로 시작하면 BoardController 실행 
+@RequestMapping("/board")
+public class BoardController {
+	//게시글목록페이지이동
+	@GetMapping("/boardList")
+	public String boardList() {
+		return "board_list"; // /WEB-INF/views/index.jsp
+	}
+}
